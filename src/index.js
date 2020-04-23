a5427c5322c1dff7ac2278785cabc008bfe091f8
“yuskillupup” <yuworkeryu55@gmail.com>
2020-04-24 00:48:43 +0900

    ×を追加

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+    constructor(props){
+      super(props);
+        this.state = {
+          value: null,
+        };
+      }
+    
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" 
+        onClick={() => {
+          this.setState({value: 'X'})
+          }}>
+          {this.state.value}
         </button>
       );
     }
-  }
+q}
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i}/>;
     }
   
     render() {
